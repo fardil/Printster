ad9400741eddada3d62402e97596fc6f01d17dd8
rividya <ivy.aluna@gmail.com>
2020-04-28 17:42:11 +0700

    layout design,size,vendor,payment,payment method,finish

---
+++ app/src/main/java/com/dilla/printster/FrontDesign.kt
@@ -0,0 +1,12 @@
+package com.dilla.printster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrontDesign : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front_design)
+    }
+}
---
+++ app/src/main/java/com/dilla/printster/backDesign.kt
@@ -0,0 +1,12 @@
+package com.dilla.printster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class backDesign : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_back_design)
+    }
+}
